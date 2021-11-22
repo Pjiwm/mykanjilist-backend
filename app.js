191f1c96318d7b6499fa474dffd74935c0dfefd4
Pjiwm <Pim.Munne@gmail.com>
2021-11-22 21:10:20 +0100

    made production ready environment

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV === 'dev') {
     mongoose.connect('mongodb://kanji_mongo/uber')
-    console.log('Connected to mongodb')
+    console.log('Connected to docker database')  
+
+} else if (process.env.NODE_ENV === 'prod') {
+    const connectionString = process.env.DATABASE_CONNECTION
+    mongoose.connect(connectionString)
+    console.log('Connected to production database')
 }
 
 app.use(bodyParser.json())
