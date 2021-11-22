2b9dd0bd4a10ddaecd46cd7beb1de19b7f598f6a
Pjiwm <Pim.Munne@gmail.com>
2021-11-22 21:19:54 +0100

    fixed routing issues

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ app.use(bodyParser.json())
 kanjiListRoutes(app)
 GuideRoutes(app)
 
+// errors
+// catch all not found response
+app.use('*', function(_, res) {
+    res.status(404).end()
+})
+
+// error responses
+app.use('*', function(err, req, res, next) {
+    console.error(`${err.name}: ${err.message}`)
+    // console.error(err)
+    next(err)
+})
+
+// app.use('*', errors.handlers)
+
+app.use('*', function(err, req, res, next) {
+    res.status(500).json({
+        message: 'something really unexpected happened'
+    })
+})
+
 app.use(async (err, req, res, next) => {
     res.status(422).send({ error: err.message })
 })
