6e6213be355ffc62023b717c221b35e432a249e7
Pjiwm <Pim.Munne@gmail.com>
2021-11-30 16:55:29 +0100

    added verify with jwt

---
+++ helpers/jwt.verify.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const secret = process.env.JWT_SECRET
+        jwt.verify(token, secret)
+        req.user = jwt.decode(token)
+        next()
+    } catch (e) {
+        return res.status(401).send({
+            code: 401,
+            error: "Unauthorized ",
+            message: "You are not signed in"
+        })
+    }
+}
